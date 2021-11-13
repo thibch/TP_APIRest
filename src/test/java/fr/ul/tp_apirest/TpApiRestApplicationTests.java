3b3149a9bbead017525fcdd2c7b657dcedadb89b
thibch <thibault.chone@protonmail.com>
2021-11-13 14:55:33 +0100

    first commit

---
+++ src/test/java/fr/ul/tp_apirest/TpApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ul.tp_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
